bab2c5ed6f1b90690e91748dab3c58169adcc152
Anton Baklanov <antonbaklanov@gmail.com>
2013-07-15 13:55:04 +0300

    fixing tests runner

--- runtests.sh
+++ runtests.sh
@@ -9,9 +9,9 @@ python setup.py clean --all
 python setup.py build --pygibson-debug || die "build failed"
 
 PYTHON_VER="`python -c 'import sys; print sys.version[:3]'`"
-BUILD_DIR="build/lib.linux-`uname -p`-$PYTHON_VER"  ## only linux is here, i know
+PLATFORM="`python -c 'from distutils import util; print util.get_platform();'`"
+BUILD_DIR="build/lib.$PLATFORM-$PYTHON_VER"
 
 echo
 echo ----------------------------------------------------------------------
-
 PYTHONPATH=$BUILD_DIR python -m tests.run
