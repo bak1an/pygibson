c2249c664cf55c820240eec7f3cf8f0179ccea88
Anton Baklanov <antonbaklanov@gmail.com>
2013-07-28 22:41:18 +0300

    testsuite now knows how to run gibson server

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,10 @@ import _pygibson
 
 import os
 import platform
+import subprocess
+import time
 from distutils.spawn import find_executable
+
 
 if platform.python_version() < '2.7':
     try:
@@ -12,15 +15,6 @@ if platform.python_version() < '2.7':
         raise
 else:
     import unittest
-
-
-_cl = _pygibson._client()
-
-
-class PyGibsonBaseTest(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        self._cl = _cl
-        super(PyGibsonBaseTest, self).__init__(*args, **kwargs)
 
 
 class ServerSpawningTestCase(unittest.TestCase):
@@ -37,12 +31,33 @@ class ServerSpawningTestCase(unittest.TestCase):
             files_dir = os.path.dirname(os.path.abspath(__file__)) + "/files/"
             if os.path.exists(files_dir + self.config_name):
                 self.config_path = files_dir + self.config_name
-        if (not self.gibson_exec or not self.config_path):
-            raise Exception("No gibson executable found on PATH or wrong config specified")
+        if not self.gibson_exec:
+            raise Exception("No gibson executable found on PATH")
+        if not self.config_path:
+            raise Exception("Wrong config specified '%s'" % self.config_name)
         super(ServerSpawningTestCase, self).__init__(*args, **kwargs)
 
+    def _getid(self):
+        return '.'.join(self.id().split('.')[2:])
+
+    def _run_gibson(self):
+        print "TESTSUITE [%s]: Starting gibson server..." % self._getid()
+        args = [self.gibson_exec, "-c", self.config_path]
+        self._p = subprocess.Popen(args)
+        print "TESTSUITE [%s]: gibson pid is '%d'" % (self._getid(), self._p.pid)
+
+    def _stop_gibson(self):
+        if self._p:
+            print "TESTSUITE [%s]: Stopping gibson server..." % self._getid()
+            self._p.terminate()
+            self._p.wait()
+            print "TESTSUITE [%s]: Stopped!" % self._getid()
+
     def setUp(self):
+        self._run_gibson()
+        time.sleep(0.5)
         super(ServerSpawningTestCase, self).setUp()
 
     def tearDown(self):
+        self._stop_gibson()
         super(ServerSpawningTestCase, self).tearDown()
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -5,13 +5,23 @@ import os
 import pygibson
 
 from . import unittest
-from . import PyGibsonBaseTest
 from . import ServerSpawningTestCase
 from . import _pygibson as pg
 
 
-@unittest.skip("Test suite is under refactoring")
-class PyGibsonExtensionTest(PyGibsonBaseTest):
+class PyGibsonExtensionTest(ServerSpawningTestCase):
+
+    def __init__(self, *args, **kwargs):
+        self.config_name = "default.conf"
+        super(PyGibsonExtensionTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        super(PyGibsonExtensionTest, self).setUp()
+        self._cl = pg._client()
+
+    def tearDown(self):
+        super(PyGibsonExtensionTest, self).tearDown()
+        self._cl = None
 
     def wait_nan(self, f):
         with self.assertRaises(pg.NaNError):
@@ -218,8 +228,12 @@ class PyGibsonExtensionTest(PyGibsonBaseTest):
         self.assertEqual(self._cl.mget(key).items(), [(key, 'atata')])
 
 
-@unittest.skip("Test suite is under refactoring")
-class TestClient(unittest.TestCase):
+class TestClient(ServerSpawningTestCase):
+
+    def __init__(self, *args, **kwargs):
+        self.config_name = "default.conf"
+        super(TestClient, self).__init__(*args, **kwargs)
+
     def test_default_ttl(self):
         c = pygibson.Client()
         c.set("client_ttl", "val")
@@ -242,7 +256,7 @@ class TestClient(unittest.TestCase):
 
 class TestUnixSockets(ServerSpawningTestCase):
     def __init__(self, *args, **kwargs):
-        self.config_name = "unix_sockets.conf"
+        self.config_name = "unix_socket.conf"
         super(TestUnixSockets, self).__init__(*args, **kwargs)
 
     def test_nothing(self):
