67431451f4eeff601ea777b9f70e10be16dedcab
Anton Baklanov <antonbaklanov@gmail.com>
2013-07-29 19:07:23 +0300

    add dummy tests filtering feature.
    now you can run parts of testsuite like that:
    ./runtests.sh test_extension.TestClient test_extension.PyGibsonExtensionTest.test_ping

--- tests/run.py
+++ tests/run.py
@@ -1,6 +1,32 @@
+import sys
+
 from . import unittest
 
+tests_to_run = []
+
+def filter_tests(t):
+    if isinstance(t, unittest.TestSuite):
+        res = unittest.TestSuite()
+        for test in t:
+            filtered = filter_tests(test)
+            if filtered:
+                res.addTest(filtered)
+        return res
+    else:
+        test_id = '.'.join(t.id().split('.')[1:])
+        for allowed_test in tests_to_run:
+            if test_id.startswith(allowed_test):
+                return t
+        return None
+
 if __name__ == "__main__":
-    suite = unittest.TestLoader().discover(".", pattern="test_*.py")
+    tests_to_run = sys.argv[1:]
+    discovered = unittest.TestLoader().discover(".", pattern="test_*.py")
+    if tests_to_run:
+        suite = unittest.TestSuite()
+        for t in discovered:
+            suite.addTest(filter_tests(discovered))
+    else:
+        suite = discovered
     unittest.TextTestRunner(verbosity=2).run(suite)
 
