6ecb554d14bf7b4e6c2a7be06d7cc8b92504a7f8
Anton Baklanov <antonbaklanov@gmail.com>
2013-08-09 00:10:03 +0300

    add long_description and classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import sys
 from distutils.core import setup, Extension
 
+DESCRIPTION = ''
+with open("README.rst", "r") as f:
+    DESCRIPTION = f.read()
+
 defines = []
 
 unprocessed_args = []
 
 for arg in sys.argv[1:]:
     if arg == "--pygibson-debug":
-        defines.append(("PYGIBSON_DEBUG", "YEAH"))
+        defines.append(('PYGIBSON_DEBUG', "YEAH"))
         continue
     unprocessed_args.append(arg)
 
@@ -28,13 +32,26 @@ _pygibson = Extension(
 
 setup(
     name='pygibson',
-    description='Python client for gibson cache server',
-    author='Anton Baklanov',
-    author_email='antonbaklanov@gmail.com',
-    url='https://github.com/bak1an/pygibson',
-    license='MIT',
+    description="Python client for gibson cache server",
+    long_description=DESCRIPTION,
+    author="Anton Baklanov",
+    author_email="antonbaklanov@gmail.com",
+    url="https://github.com/bak1an/pygibson",
+    license="MIT",
     ext_modules=[_pygibson],
-    package_dir={'': 'src'},
+    package_dir={'': "src"},
     py_modules=['pygibson'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries"
+    ],
     version="0.1.0"
 )
