089f403fe665fb9aeb2c6dccacd21f5ae1a95003
Anton Baklanov <antonbaklanov@gmail.com>
2013-08-18 00:53:22 +0300

    testsuite now runs with py3.

--- runtests.sh
+++ runtests.sh
@@ -14,8 +14,8 @@ fi;
 python setup.py clean --all
 python setup.py build --pygibson-debug || die "build failed"
 
-PYTHON_VER="`python -c 'import sys; print sys.version[:3]'`"
-PLATFORM="`python -c 'from distutils import util; print util.get_platform();'`"
+PYTHON_VER="`python -c 'import sys; sys.stdout.write(sys.version[:3])'`"
+PLATFORM="`python -c 'import sys; from distutils import util; sys.stdout.write(util.get_platform());'`"
 BUILD_DIR="build/lib.$PLATFORM-$PYTHON_VER"
 
 echo
